2733d0f509e2bff598ed56bdd09bc9c37777560e
danielwisouza <daniel2wis@gmail.com>
2019-06-05 10:28:32 -0300

    Atualização #03

--- ServerCorreiosRastreamento/src/main/java/hello/Modelo.java
+++ ServerCorreiosRastreamento/src/main/java/hello/Modelo.java
@@ -1,7 +1,5 @@
 package hello;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.NoSuchElementException;
+
 
 import com.db4o.Db4oEmbedded;
 import com.db4o.ObjectContainer;
@@ -9,52 +7,26 @@ import com.db4o.ObjectSet;
 import com.db4o.query.Query;
 
 public class Modelo {
-	ObjectContainer Administrador = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "bd/Administrador.db4o");
+	ObjectContainer Usuario = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "bd/Usuario.db4o");
 	
-
-	public boolean addAdministrador(Administrador Administrador){
-		if(isAdministradorUserAvailable(Administrador.getUsername())){
-			
-			this.Administrador.store(Administrador);
-			this.Administrador.commit();
-			
-			return true;
-		}
-		
-		return false;
-	}	
 	
-	private boolean isAdministradorUserAvailable(String username) {
-		Query query = Administrador.query();
-		query.constrain(Administrador.class);
-	    ObjectSet<Administrador> allAdministrador = query.execute();
-	    
-	    for(Administrador Administrador:allAdministrador){
-	    	if(Administrador.getUsername().equals(username)) return false;
-	    }
-	    
-	    return true;
-	}
-	
-	/*
-	 * //--------------------- USUARIOS --------------------- public boolean
-	 * addUsuario(Usuario Usuario){ if(isUsuarioUserAvailable(Usuario.getEmail())){
-	 * 
-	 * this.Usuario.store(Usuario); this.Usuario.commit();
-	 * 
-	 * return true; }
-	 * 
-	 * return false; }
-	 * 
-	 * 
-	 * private boolean isUsuarioUserAvailable(String email) { Query query =
-	 * Usuario.query(); query.constrain(Usuario.class); ObjectSet<Usuario>
-	 * allUsuario = query.execute();
-	 * 
-	 * for(Usuario Usuario:allUsuario){ if(Usuario.getEmail().equals(email)) return
-	 * false; }
-	 * 
-	 * return true; }
-	 */
+	  boolean addUsuario(Usuario Usuario){ if(isUsuarioUserAvailable(Usuario.getEmail())){
+	  
+	  this.Usuario.store(Usuario); this.Usuario.commit();
+	  
+	  return true; }
+	  
+	  return false; }
+	  
+	  
+	  private boolean isUsuarioUserAvailable(String email) { Query query =
+	  Usuario.query(); query.constrain(Usuario.class); ObjectSet<Usuario>
+	  allUsuario = query.execute();
+	  
+	  for(Usuario Usuario:allUsuario){ if(Usuario.getEmail().equals(email)) return
+	  false; }
+	  
+	  return true; }
+	 
 	
 }
--- ServerCorreiosRastreamento/src/main/java/hello/Usuario.java
+++ ServerCorreiosRastreamento/src/main/java/hello/Usuario.java
@@ -1,43 +1,21 @@
 package hello;
 
 public class Usuario {
-	@Override
-	public String toString() {
-		return "Usuario [nome=" + nome + ", email=" + email + ", username=" + username + ", password=" + password
-				+ ", tipoUsuario=" + tipoUsuario + "]";
-	}
-	private String nome;
 	private String email;
-	private String username;
 	private String password;
-	private String tipoUsuario;
+
 	
 	
-public Usuario(String nome, String email, String username, String password, String tipoUsuario) {
-	this.nome = nome;
+public Usuario(String nome, String email, String password) {
 	this.email = email;
-	this.username = username;
 	this.password = password;
-	this.tipoUsuario = tipoUsuario;
 	}
 
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
 	}
 	public String getPassword() {
 		return password;
@@ -45,12 +23,6 @@ public Usuario(String nome, String email, String username, String password, Stri
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public String getTipoUsuario() {
-		return tipoUsuario;
-	}
-	public void setTipoUsuario(String tipoUsuario) {
-		this.tipoUsuario = tipoUsuario;
-	}
-	
+
 }
 
